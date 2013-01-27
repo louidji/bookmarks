2b30865b386f015f8322f3bd632ba0b29e316f2c
Louis TOURNAYRE <louidji@gmail.com>
2013-01-27 23:29:14 +0100

    Ajout catégories

---
+++ app/models/Category.scala
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+package models
+
+/**
+ * @author louis
+ *
+ */
+case class Category (id: Long, label: String)
+
+object Category {
+  
+  def all(): List[Category] = Nil
+  
+  def create(label: String) {}
+  
+  def delete(id: Long) {}
+  
+  def find(id:Long): Category = {
+    Category(id, "TODO")
+  }
+  
+}

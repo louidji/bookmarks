2f0eaec3e58a479c5c24f733642e66f86be0b965
Louis TOURNAYRE <louidji@gmail.com>
2013-02-23 22:59:53 +0100

    Ajout d'un filtre de sécu simple

--- app/Global.scala
+++ app/Global.scala
@@ -6,9 +6,11 @@
  * To change this template use File | Settings | File Templates.
  */
 
+import controllers.routes
 import play.api._
+import play.api.mvc._
 
-object Global extends GlobalSettings {
+object Global extends WithFilters(AuthorizedFilter("index", "bookmarks", "categories",  "delete", "edit", "save")) with GlobalSettings {
   override def onStart(app: Application) {
     Logger.info("Application has started")
   }
@@ -18,4 +20,46 @@ object Global extends GlobalSettings {
   }
 
 
+}
+
+object AccessLog extends Filter {
+  override def apply(next: RequestHeader => Result)(request: RequestHeader): Result = {
+    val result = next(request)
+    play.Logger.info(request + "\n\t => " + result)
+    result
+  }
+}
+
+object AuthorizedFilter {
+  def apply(actionNames: String*) = new AuthorizedFilter(actionNames)
+}
+
+class AuthorizedFilter(actionNames: Seq[String]) extends Filter {
+
+  /**
+   * Retrieve the connected user email.
+   */
+  private def username(request: RequestHeader) = request.session.get(Security.username)
+
+  override def apply(next: RequestHeader => Result)(request: RequestHeader): Result = {
+    if (authorizationRequired(request)) {
+      /* do the auth stuff here */
+      val user = username(request)
+      play.Logger.info("auth required for : <" + user + "> ?")
+      user match {
+        case Some(_) => next(request)
+        case None => play.api.mvc.Results.Redirect(routes.Application.login)
+
+      }
+
+
+    }
+    else next(request)
+  }
+
+  private def authorizationRequired(request: RequestHeader) = {
+    play.Logger.info("SEC ? : " + request)
+    val actionInvoked: String = request.tags.getOrElse(play.api.Routes.ROUTE_ACTION_METHOD, "")
+    actionNames.contains(actionInvoked)
+  }
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,7 +8,16 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 
-object Application extends Controller {
+object Application extends Controller  {
+
+  val loginForm = Form(
+    tuple(
+      "email" -> text,
+      "password" -> text
+    ) verifying ("Invalid email or password", result => result match {
+      case (email, password) => check(email, password)
+    })
+  )
 
   def index = Action {
     Ok(views.html.index(Bookmark.allFull()))
@@ -16,4 +25,26 @@ object Application extends Controller {
 
 
 
+  def check(username: String, password: String) = {
+    (username == "admin@cgi.com" && password == "1234")
+  }
+
+  def login = Action { implicit request =>
+    Ok(views.html.login(loginForm))
+  }
+
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.login(formWithErrors)),
+      user => {
+        Redirect(routes.Application.index).withSession(Security.username -> user._1)
+      }
+    )
+  }
+
+  def logout = Action {
+    Redirect(routes.Application.login).withNewSession.flashing(
+      "success" -> "You are now logged out."
+    )
+  }
 }
